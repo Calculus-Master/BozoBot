111637cc14b23600886be9f2fcfadf1fcc7e4c50
Saptarshi Mallick <sapmallick@gmail.com>
2023-07-21 21:11:21 -0700

    Message repeater is now a 0.5% chance

--- src/main/java/com/calculusmaster/bozo/util/Listener.java
+++ src/main/java/com/calculusmaster/bozo/util/Listener.java
@@ -151,7 +151,7 @@ public class Listener extends ListenerAdapter
             event.getChannel().sendMessage("i'm happy for you tho").queue();
             event.getChannel().sendMessage("or sorry that happened").queue();
         }
-        else if(content.length() <= 100 && r.nextFloat() < 0.05F)
+        else if(content.length() <= 100 && r.nextFloat() < 0.005F)
         {
             StringBuilder modified = new StringBuilder();
             for(char c : event.getMessage().getContentRaw().toCharArray()) modified.append(r.nextBoolean() ? String.valueOf(c).toUpperCase() : String.valueOf(c).toLowerCase());
