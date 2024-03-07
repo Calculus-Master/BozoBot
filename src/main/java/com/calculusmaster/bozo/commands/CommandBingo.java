84530fa975d4afe4460597e265b2252a7fac731a
Saptarshi Mallick <sapmallick@gmail.com>
2024-03-06 20:32:08 -0700

    Added buttons and pagination to /bingo list, /bingo add now shows the entry number

--- src/main/java/com/calculusmaster/bozo/commands/CommandBingo.java
+++ src/main/java/com/calculusmaster/bozo/commands/CommandBingo.java
@@ -3,23 +3,33 @@ package com.calculusmaster.bozo.commands;
 import com.calculusmaster.bozo.commands.core.Command;
 import com.calculusmaster.bozo.commands.core.CommandData;
 import com.calculusmaster.bozo.util.BingoManager;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.InteractionHook;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.utils.SplitUtil;
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class CommandBingo extends Command
 {
+    public static final int PAGE_SIZE = 15;
+    private static final Button PREV_BUTTON = Button.secondary("bingo_list_previous", Emoji.fromFormatted("⬅"));
+    private static final Button NEXT_BUTTON = Button.secondary("bingo_list_next", Emoji.fromFormatted("➡"));
+
+    private static final Map<String, BingoListMessage> LIST_MESSAGES = new HashMap<>();
+    private record BingoListMessage(String messageID, int page, List<String> filteredList) {}
+
     public static void init()
     {
         CommandData
@@ -36,7 +46,8 @@ public class CommandBingo extends Command
                                         .addOption(OptionType.INTEGER, "number", "The entry number to edit. Use /bingo list to see the entries.", true)
                                         .addOption(OptionType.STRING, "entry", "The new entry.", true),
                                 new SubcommandData("list", "List all possible Bingo Board entries.")
-                                        .addOption(OptionType.STRING, "search", "Search for entries that contain this string.", false),
+                                        .addOption(OptionType.STRING, "search", "Search for entries that contain this string.", false)
+                                        .addOption(OptionType.INTEGER, "page", "The page number to view.", false),
                                 new SubcommandData("complete", "Mark a square as completed.")
                                         .addOption(OptionType.STRING, "square", "The square (A1, B3, etc.) to mark as completed. The letter is the row and the number is the column.", true, true),
                                 new SubcommandData("board", "View the current Bingo Board."),
@@ -49,6 +60,7 @@ public class CommandBingo extends Command
                                 new SubcommandData("list-free-spaces", "List all entries that can be free spaces.")
                         )
                 )
+                .withButtons(PREV_BUTTON.getId(), NEXT_BUTTON.getId())
                 .register();
     }
 
@@ -68,7 +80,7 @@ public class CommandBingo extends Command
 
             BingoManager.addEntry(entry);
 
-            this.response = "Added `" + entry + "` to the Bingo Board.";
+            this.response = "Added `" + entry + "` (#" + BingoManager.ENTRIES.size() + ") to the Bingo Board.";
         }
         else if(subcommand.equals("delete"))
         {
@@ -98,25 +110,46 @@ public class CommandBingo extends Command
             if(BingoManager.ENTRIES.isEmpty()) return this.error("No entries in the Bingo Board!");
 
             String search = event.getOption("search") != null ? event.getOption("search").getAsString() : "";
+            int page = event.getOption("page") != null ? event.getOption("page").getAsInt() : 1;
 
             List<String> list = new ArrayList<>();
-
             for(int i = 0; i < BingoManager.ENTRIES.size(); i++)
             {
                 String e = BingoManager.ENTRIES.get(i);
 
                 if(search.isEmpty() || e.toLowerCase().contains(search.toLowerCase()))
-                    list.add((i + 1) + ": " + e);
+                    list.add("**" + (i + 1) + "**: " + e);
             }
 
-            String out = String.join("\n", list);
+            int totalPageCount = this.getTotalPageCount(list.size());
+            if(page <= 0 || page > totalPageCount) return this.error("Invalid page number!");
 
-            List<String> split = SplitUtil.split(out, 2000, SplitUtil.Strategy.NEWLINE);
+            if(list.isEmpty()) return this.error("No entries found.");
 
-            //Send first as a reply, send remaining as messages
-            event.reply(split.get(0)).queue();
-            for(int i = 1; i < split.size(); i++)
-                event.getChannel().sendMessage(split.get(i)).queue();
+            int start = (page - 1) * PAGE_SIZE;
+            int end = Math.min(start + Math.min(PAGE_SIZE, list.size() - start), BingoManager.ENTRIES.size());
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setDescription(String.join("\n", list.subList(start, end)));
+            embed.setFooter("Page " + page + " of " + totalPageCount);
+
+            List<Button> buttons = new ArrayList<>();
+            if(page - 1 > 0) buttons.add(PREV_BUTTON);
+            if(page < totalPageCount) buttons.add(NEXT_BUTTON);
+
+            Consumer<InteractionHook> cacher = ih -> ih.retrieveOriginal().onSuccess(
+                    m -> LIST_MESSAGES.put(m.getId(), new BingoListMessage(m.getId(), page, list))
+            ).queue();
+
+            if(buttons.isEmpty())
+                event.replyEmbeds(embed.build())
+                        .queue(cacher);
+            else
+                event.replyEmbeds(embed.build())
+                        .setActionRow(buttons)
+                        .queue(cacher);
+
+            this.response = "";
         }
         else if(subcommand.equals("complete"))
         {
@@ -227,5 +260,42 @@ public class CommandBingo extends Command
             return true;
         }
         else return this.error("Something went wrong.");
+    }
+
+    @Override
+    protected boolean buttonLogic(ButtonInteractionEvent event)
+    {
+        if(event.getComponentId().equals(PREV_BUTTON.getId()) || event.getComponentId().equals(NEXT_BUTTON.getId()))
+        {
+            boolean prev = event.getComponentId().equals(PREV_BUTTON.getId());
+            BingoListMessage current = LIST_MESSAGES.get(event.getMessageId());
+
+            int newPage = prev ? current.page - 1 : current.page + 1;
+            if(newPage <= 0 || newPage > this.getTotalPageCount(current.filteredList().size())) return this.error("Invalid page number!");
+
+            BingoListMessage newLM = new BingoListMessage(current.messageID, newPage, current.filteredList);
+            int start = (newPage - 1) * PAGE_SIZE;
+            int end = Math.min(start + Math.min(PAGE_SIZE, current.filteredList.size() - start), BingoManager.ENTRIES.size());
+
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.setDescription(String.join("\n", current.filteredList.subList(start, end)));
+            embed.setFooter("Page " + newPage + " of " + this.getTotalPageCount(current.filteredList.size()));
+
+            List<Button> buttons = new ArrayList<>();
+            if(newPage - 1 > 0) buttons.add(PREV_BUTTON);
+            if(newPage < this.getTotalPageCount(current.filteredList.size())) buttons.add(NEXT_BUTTON);
+
+            if(buttons.isEmpty()) event.editMessageEmbeds(embed.build()).queue();
+            else event.editMessageEmbeds(embed.build()).setActionRow(buttons).queue();
+
+            LIST_MESSAGES.put(event.getMessageId(), newLM);
+        }
+
+        return true;
+    }
+
+    private int getTotalPageCount(int listSize)
+    {
+        return listSize / PAGE_SIZE + 1;
     }
 }
